e5c4b072606fbf1e59bd6cb4f88e30bfc40ceaae
ibehanwillett <imogenbdubs@gmail.com>
2023-10-19 10:48:27 +1000

    Changed the structure of my room classes.

--- terminal-app/main.py
+++ terminal-app/main.py
@@ -1,59 +1,71 @@
 from abc import ABC, abstractmethod
 
-
+# Room base class
 class Room(ABC):
     def __init__(self):
         self.name = name
-        self.description = description
-        hasNorthDoor = False
-        hasEastDoor = False
-        hasSouthDoor = False
-        hasWestDoor = False
-
-    @abstractmethod
-    def what_doors(self, name):
-        
-        
-
-
-    @abstractmethod
-    def go(self, direction):
-        pass
+        NorthDoor = False
+        EastDoor = False
+        SouthDoor = False
+        WestDoor = False
     
-class Entrance(Room):
-    def __init__(self):
-        self.description = "You are at the entrance of the house."
-        hasEastDoor = True
-
+    def __str__(self):
+        return f'You are in the {self.name}'
+    
+    # @abstractmethod
+    # def what_doors(self, name):
+    #     pass
         
+
+    # @abstractmethod
+    # def go(self, direction):
+    #     pass
+    
+# Rooms themselves
+class Entrance(Room):
+    def __init__(self, name):
+        self.name = name
+        EastDoor = True
+        SouthDoor = True
+        WestDoor = True
+
 
 class Library(Room):
-    def __init__(self):
-        self.description = "You are in the library"
+    def __init__(self, name):
+        self.name = name
 
-class Study(Room):
-    def __init__(self):
-        self.description = "You are in the study."
+# class Study(Room):
+#     def __init__(self, name):
+#         self.description = "You are in the study."
 
-class Statue(Room):
-    def __init__(self):
-        self.description = "You are in a room full of statues."
+# class Statue(Room):
+#     def __init__(self):
+#         self.description = "You are in a room full of statues."
 
-class Bedroom(Room):
-    def __init__(self):
-        self.description = "You are in the bedroom."
+# class Bedroom(Room):
+#     def __init__(self):
+#         self.description = "You are in the bedroom."
 
-class Kitchen(Room):
-    def __init__(self):
-        self.description = "You are in the kitchen."
+# class Kitchen(Room):
+#     def __init__(self):
+#         self.description = "You are in the kitchen."
 
-class DininigRoom(Room):
-    def __init__(self):
-        self.description = "You are in the kitchen ."
+# class DininigRoom(Room):
+#     def __init__(self):
+#         self.description = "You are in the kitchen ."
 
-        def doorway(direction, room):
-            print(f"The {room} is to the {direction}")
+#         def doorway(direction, room):
+#             print(f"The {room} is to the {direction}")
 
+# MOVEMENT
+def doorways():
+    if  EastDoor = True
+        SouthDoor = True
+        WestDoor = True
 
+#START OF GAME
 print("You wake in a spooky scary house! Uh oh!")
 
+# ENTRANCE 
+entrance = Entrance("Entrance")
+print(entrance)
