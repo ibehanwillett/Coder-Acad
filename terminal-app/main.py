79a47c87eecd4eb678eabf7dabdc7f6cc8a6b49c
ibehanwillett <imogenbdubs@gmail.com>
2023-10-19 14:08:35 +1000

    Futher experiments on what the most effienct way to structure movement between rooms.

--- terminal-app/main.py
+++ terminal-app/main.py
@@ -1,38 +1,53 @@
 from abc import ABC, abstractmethod
 
+# GENERAL SCRIPT
+howto_move = print('To move to another room, input the direction you want to move)')
+position = 'entrance'
+
 # Room base class
 class Room(ABC):
     def __init__(self):
         self.name = name
-        NorthDoor = False
-        EastDoor = False
-        SouthDoor = False
-        WestDoor = False
+        NorthDoor = 'locked'
+        EastDoor = 'locked'
+        SouthDoor = 'locked'
+        WestDoor = 'locked'
     
     def __str__(self):
         return f'You are in the {self.name}'
-    
-    # @abstractmethod
-    # def what_doors(self, name):
-    #     pass
-        
 
-    # @abstractmethod
-    # def go(self, direction):
-    #     pass
+
+# Doors
+class Door():
+    def __init__(self, name):
+        self.name: name
+
+    def move(name):
+        # global position = name
+        print(f'You move into the {name}')
+            
+         
+
+
     
-# Rooms themselves
+# Rooms themselves definition
 class Entrance(Room):
     def __init__(self, name):
         self.name = name
-        EastDoor = True
-        SouthDoor = True
-        WestDoor = True
+        
+
+    def description(self):
+       print('The entrance is a room. Placeholder description. There is a door on the east wall, west wall and south wall.')
+    
 
 
 class Library(Room):
     def __init__(self, name):
         self.name = name
+        
+
+    def description(self):
+       print('You\'re in a libray. Placeholder description. There is a door on south wall and west wall.')
 
 # class Study(Room):
 #     def __init__(self, name):
@@ -57,15 +72,42 @@ class Library(Room):
 #         def doorway(direction, room):
 #             print(f"The {room} is to the {direction}")
 
-# MOVEMENT
-def doorways():
-    if  EastDoor = True
-        SouthDoor = True
-        WestDoor = True
+
+
+
+
+def whereto():
+    direction = str(input("What direction would you like to go?"))
+    return direction
+
+
+
+
+
+#SET UP
+## Rooms set-up
+entrance = Entrance('Entrance')
+entrance.east_door = Door('library')
+library = Library('Library')
+library.west_door = Door('entrance')
+
+## Doors set-up
 
 #START OF GAME
-print("You wake in a spooky scary house! Uh oh!")
+print('You wake in a spooky scary house! Uh oh!')
 
 # ENTRANCE 
-entrance = Entrance("Entrance")
-print(entrance)
+entrance.description()
+print(howto_move)
+move_choice = whereto()
+if move_choice == 'east':
+    library.move()
+else:
+    print('I have\'t coded that yet, sorry!')
+
+    
+    
+
+
+    
+
