2b572b5e1c0d6f709463229eec8e31b242ad0067
ibehanwillett <imogenbdubs@gmail.com>
2023-11-16 12:46:16 +1000

    Added to the trello clone app. Added module Marshmallow.

--- flask-lessons/trello-clone/app.py
+++ flask-lessons/trello-clone/app.py
@@ -1,41 +1,74 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from datetime import date
+from flask_marshmallow import Marshmallow
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] ='postgresql+psycopg2://trello_dev:spameggs123@127.0.0.1:5432/trello' # Connector string
 
 db = SQLAlchemy(app)
+ma = Marshmallow(app)
 
 class Card(db.Model):
-    __tablename__ = 'cards'
-    
+    __tablename__ = "cards"
+
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100))
     description = db.Column(db.Text())
+    status = db.Column(db.String(30))
     date_created = db.Column(db.Date())
 
-@app.cli.command('db_create')
+class CardSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'title', 'description', 'status', 'date_created')
+
+
+@app.cli.command("db_create")
 def db_create():
     db.drop_all()
     db.create_all()
-    print('Created tables')
+    print("Created tables")
 
-@app.cli.command('db_seed')
+
+@app.cli.command("db_seed")
 def db_seed():
-    card = Card(
-        title = 'Start the project',
-        description = 'Stage 1- Create ERD',
-        date_created = date.today(),
-    ) #Just creates instance in RAM, not in database.
+    cards = [
+        Card(
+            title="Start the project",
+            description="Stage 1 - ERD Creation",
+            status='Done',
+            date_created=date.today(),
+        ),
+        Card(
+            title="ORM Queries",
+            description="Stage 2 - Implement CRUD queries",
+            status='In Progress',
+            date_created=date.today(),
+        ),
+        Card(
+            title="Marshmallow",
+            description="Stage 3 - Implement JSONify of models",
+            status='In Progress',
+            date_created=date.today(),
+        ),
+    ]
 
-    db.session.add(card) #This adds to db (transaction begins)
-    db.session.commit() #transaction ends
+    db.session.add_all(cards)
+    db.session.commit()
 
-    print('Database seeded')
+    print("Database seeded")
 
 
-@app.route('/')
+@app.route('/cards')
+def all_cards():
+    # select * from cards;
+    stmt = db.select(Card).where(db.or_(Card.status != 'Done', Card.id > 2)).order_by(Card.title.desc())
+    cards = db.session.scalars(stmt).all()
+    return CardSchema(many=True).dump(cards)
+
+
+
+@app.route("/")
 def index():
-    return 'Hello world!' 
+    return "Hello world!"
