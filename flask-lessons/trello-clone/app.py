1d1dda812f625cf98911c4fb17db96d8abe4c19b
ibehanwillett <imogenbdubs@gmail.com>
2023-11-21 12:28:03 +1000

    Added users to trello clone

--- flask-lessons/trello-clone/app.py
+++ flask-lessons/trello-clone/app.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from datetime import date
 from flask_marshmallow import Marshmallow
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 
@@ -9,6 +10,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] ='postgresql+psycopg2://trello_dev:spamegg
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
+bcrypt= Bcrypt(app)
 
 class Card(db.Model):
     __tablename__ = "cards"
@@ -23,6 +25,15 @@ class CardSchema(ma.Schema):
     class Meta:
         fields = ('id', 'title', 'description', 'status', 'date_created')
 
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    email = db.Column(db.String, nullable=False, unique=True)
+    password = db.Column(db.String, nullable=False)
+    is_admin = db.Column(db.Boolean, default=False)
+# default will be handled by Alchemy.
 
 @app.cli.command("db_create")
 def db_create():
@@ -33,6 +44,19 @@ def db_create():
 
 @app.cli.command("db_seed")
 def db_seed():
+    users= [
+        User(
+            email='admin@spam.com',
+            password=bcrypt.generate_password_hash('swordfish').decode('utf8'),
+            is_admin=True
+        ),
+        User(
+            name='John Cleese',
+            email='cleese@spam.com',
+            password=bcrypt.generate_password_hash('tisbutascratch').decode('utf8'),
+        )
+    ]
+    
     cards = [
         Card(
             title="Start the project",
@@ -54,6 +78,7 @@ def db_seed():
         ),
     ]
 
+    db.session.add_all(users)
     db.session.add_all(cards)
     db.session.commit()
 
