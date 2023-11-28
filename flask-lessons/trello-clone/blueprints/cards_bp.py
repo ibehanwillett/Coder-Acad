19c36f6144cb1acbafbec4fa879887e448c4dc95
ibehanwillett <imogenbdubs@gmail.com>
2023-11-28 14:58:57 +1000

    Added remaining CRUD methods to cards route

--- flask-lessons/trello-clone/blueprints/cards_bp.py
+++ flask-lessons/trello-clone/blueprints/cards_bp.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, abort
 from setup import db
 from models.card import Card, CardSchema
 from flask_jwt_extended import jwt_required
@@ -19,6 +19,7 @@ def all_cards():
     return CardSchema(many=True).dump(cards)
 
 @cards_bp.route('/<int:id>/')
+@jwt_required()
 def one_card(id):
     admin_required()
     stmt = db.select(Card).filter_by(id=id)
@@ -29,7 +30,45 @@ def one_card(id):
         return {'error':'Card not found'}, 404
 
 @cards_bp.route('/', methods=['POST'])
+@jwt_required()
 def create_card():
     card_info = CardSchema(exclude=['id','date_created']).load(request.json)
-    print(card_info)
-    return {}
+    card = Card(
+        title=card_info['title'],
+        description=card_info.get('description',''),
+        status=card_info.get('status', 'To Do')
+    )
+    db.session.add(card)
+    db.session.commit()
+    return CardSchema().dump(card),201
+
+# Update a card
+@cards_bp.route('/<int:id>', methods=['PUT','PATCH'])
+@jwt_required()
+def update_card(id):
+    admin_required()
+    card_info = CardSchema(exclude=['id','date_created']).load(request.json)
+    stmt = db.select(Card).filter_by(id=id)
+    card = db.session.scalar(stmt)
+    if card: # If card is a truthy value, return
+        card.title = card_info.get('title', card.title)
+        card.description = card_info.get('description', card.description)
+        card.status = card_info.get('status', card.status)
+        db.session.commit()
+        return CardSchema().dump(card)
+    else:
+        return {'error':'Card not found'}, 404
+    
+# Delete a card
+@cards_bp.route('/<int:id>', methods=['DELETE'])
+@jwt_required()
+def delete_card(id):
+    admin_required()
+    stmt = db.select(Card).filter_by(id=id)
+    card = (db.session.scalar(stmt))
+    if card:
+        db.session.delete(card)
+        db.session.commit()
+        abort(200)
+    else:
+        return {'error':'Card not found'}, 404
