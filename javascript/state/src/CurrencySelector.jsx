7e93ec2c982b92090dd06e300dc35522039b230f
ibehanwillett <imogenbdubs@gmail.com>
2024-01-31 12:30:05 +1000

    Increased functionality of drop down box

--- javascript/state/src/CurrencySelector.jsx
+++ javascript/state/src/CurrencySelector.jsx
@@ -2,14 +2,18 @@ import React, { useEffect, useState } from 'react'
 
 const CurrencySelector = ( { setCurrency }) => {
     const [selectValue, setSelectValue] = useState("AUD")
+    const [currencies, setCurrencies] = useState([])
 
     useEffect(() => setCurrency(selectValue), [selectValue])
+    useEffect(() => {
+        fetch('https://api.coindesk.com/v1/bpi/supported-currencies.json')
+        .then(res => res.json())
+        .then(data => setCurrencies(data))
+    }, [])
 
   return (
     <select onChange= {(event) => setSelectValue(event.target.value)} value={selectValue}>
-        <option value="AUD">Australian Dollar</option>
-        <option value="USD">US Dollar</option>
-        <option value="EUR">Euro</option>
+        {currencies.map(cur => <option key={cur.currency} value={cur.currency}>{cur.country}</option>)}
     </select>
   )
 }
