e8bcca78ac15682a18bc26e37c8b450b84c418d1
ibehanwillett <imogenbdubs@gmail.com>
2024-02-05 11:25:47 +1000

    created routes. began styling with bulma

--- javascript/journal/src/components/App.jsx
+++ javascript/journal/src/components/App.jsx
@@ -1,20 +1,30 @@
 import { useState } from 'react'
 import reactLogo from '../assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 import Home from './Home.jsx'
 import NewEntry from './NewEntry.jsx'
 import CategorySelection from './CategorySelection.jsx'
+import NavBar from './NavBar.jsx'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
+      
+      <BrowserRouter>
+      <NavBar />
       <h1>Journal</h1>
-      <Home />
-      <NewEntry />
-      <CategorySelection />
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/category' element={<CategorySelection/>}/>
+          <Route path='/entry'>
+            <Route path='new' element={<NewEntry/>}/>
+          </Route>
+          <Route path='*' element={<h2>Page Not Found</h2>}/>
+          
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
